1cb20b944ac521759f35e1cf437be6de80ea5c48
enaikele omoh kelvin <enaikeleomoh@gmail.com>
2023-07-21 00:13:00 +0100

    Refactored

--- assets/img/Ecpres technologies_files/main.js
+++ assets/img/Ecpres technologies_files/main.js
@@ -62,57 +62,29 @@ window.addEventListener('scroll', scrollUp)
 /*=============== DARK LIGHT THEME ===============*/
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
-const iconTheme = 'bx-toggle-rightg'
+const iconTheme = 'bx-toggle-right'
 
-// // Previously selected topic (if user selected)
-// const selectedTheme = localStorage.getItem('selected-theme')
-// const selectedIcon = localStorage.getItem('selected-icon')
-
-// // We obtain the current theme that the interface has by validating the dark-theme class
-// const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-// const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'fa-solid fa-toggle-off' : 'fa-solid fa-toggle-on'
-
-// // We validate if the user previously chose a topic
-// if (selectedTheme) {
-//   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-//   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-//   themeButton.classList[selectedIcon === 'fa-solid fa-toggle-ff' ? 'add' : 'remove'](iconTheme)
-// }
-
-// // Activate / deactivate the theme manually with the button
-// themeButton.addEventListener('click', () => {
-//     // Add or remove the dark / icon theme
-//     document.body.classList.toggle(darkTheme)
-//     themeButton.classList.toggle(iconTheme)
-//     // We save the theme and the current icon that the user chose
-//     localStorage.setItem('selected-theme', getCurrentTheme())
-//     localStorage.setItem('selected-icon', getCurrentIcon())
-// })
-
-
-
-
-
+// Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
 
 // We obtain the current theme that the interface has by validating the dark-theme class
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'fa-solid fa-toggle-off' : 'fa-solid fa-toggle-on'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-toggle-left' : 'bx-toggle-right'
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'fa-solid fa-toggle-off' ? 'add' : 'remove'](iconTheme)
+  themeButton.classList[selectedIcon === 'bx-toggle-left' ? 'add' : 'remove'](iconTheme)
 }
 
 // Activate / deactivate the theme manually with the button
 themeButton.addEventListener('click', () => {
-  // Add or remove the dark / icon theme
-  document.body.classList.toggle(darkTheme)
-  themeButton.classList.toggle(iconTheme)
-  // We save the theme and the current icon that the user chose
-  localStorage.setItem('selected-theme', getCurrentTheme())
-  localStorage.setItem('selected-icon', getCurrentIcon())
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
 })
